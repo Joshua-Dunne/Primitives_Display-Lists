0d205ef7e89c0bc8fd3c5e2ed204a36db58836cc
Joshua Dunne <C00241588@itcarlow.ie>
2019-12-09 21:07:44 +0000

    Implemented glLists so now Primative shapes can be displayed individually. Each shape can be cycled through using Numpad 4 and Numpad 6, for Next Shape and Previous shape respectively.

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -1,7 +1,114 @@
 #include <Game.h>
+//
+//Game::Game() : window(VideoMode(800, 600), "OpenGL")
+//{
+//}
+//
+//Game::~Game() {}
+//
+//void Game::run()
+//{
+//
+//	initialize();
+//
+//	Event event;
+//
+//	while (isRunning) {
+//
+//		cout << "Game running..." << endl;
+//
+//		while (window.pollEvent(event))
+//		{
+//			if (event.type == Event::Closed)
+//			{
+//				isRunning = false;
+//			}
+//		}
+//		update();
+//		draw();
+//	}
+//
+//}
+//
+//void Game::initialize()
+//{
+//	isRunning = true;
+//
+//	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+//	glMatrixMode(GL_PROJECTION);
+//	glLoadIdentity();
+//	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0);
+//	glMatrixMode(GL_MODELVIEW);
+//}
+//
+//void Game::update()
+//{
+//	cout << "Update up" << endl;
+//		if (Keyboard::isKeyPressed(Keyboard::Up))
+//		{
+//			glTranslatef(0, 0.1f, 0);
+//		}
+//		if (Keyboard::isKeyPressed(Keyboard::Down))
+//		{
+//			glTranslatef(0, -0.1f, 0);
+//		}
+//		if (Keyboard::isKeyPressed(Keyboard::Left))
+//		{
+//			glTranslatef(-0.1f, 0, 0);
+//		}
+//		if (Keyboard::isKeyPressed(Keyboard::Right))
+//		{
+//			glTranslatef(0.1f, 0, 0);
+//		}
+//
+//		// X Axis
+//		if (Keyboard::isKeyPressed(Keyboard::X))
+//		{
+//			glRotatef(1.0f, 0.1f, 0, 0);
+//		} 
+//		else if (Keyboard::isKeyPressed(Keyboard::C))
+//		{
+//			glRotatef(1.0f, -0.1f, 0, 0);
+//		}
+//
+//		// Y Axis
+//		if (Keyboard::isKeyPressed(Keyboard::S))
+//		{
+//			glRotatef(1.0f, 0, 0.1f, 0);
+//		}
+//		else if (Keyboard::isKeyPressed(Keyboard::D))
+//		{
+//			glRotatef(1.0f, 0, -0.1f, 0);
+//		}
+//
+//		// Z Axis
+//		if (Keyboard::isKeyPressed(Keyboard::W))
+//		{
+//			glRotatef(1.0f, 0, 0, 0.1f);
+//		}
+//		else if (Keyboard::isKeyPressed(Keyboard::E))
+//		{
+//			glRotatef(1.0f, 0, 0, -0.1f);
+//		}
+//
+//		// scale on P
+//		if (Keyboard::isKeyPressed(Keyboard::P))
+//		{
+//			glScalef(0.99f, 0.99f, 1);
+//		}
+//		else if (Keyboard::isKeyPressed(Keyboard::O))
+//		{
+//			glScalef(1.01f, 1.01f, 1);
+//		}
+//}
+//
 
-Game::Game() : window(VideoMode(800, 600), "OpenGL")
+Game::Game() : window(VideoMode(800, 600), "OpenGL"), primatives(9)
 {
+	 // uncomment for part 2
+	 // ********************
+	 index = glGenLists(primatives);
+	 // ********************
 }
 
 Game::~Game() {}
@@ -23,6 +130,48 @@ void Game::run()
 			{
 				isRunning = false;
 			}
+
+			if (event.type == Event::KeyPressed && !m_keyHeld)
+			{
+				switch (event.key.code)
+				{
+				case Keyboard::Space:
+					glLoadIdentity();
+					m_keyHeld = true;
+					break;
+				case Keyboard::Numpad4:
+					if (current > 1)
+					{
+						current--;
+					}
+					m_keyHeld = true;
+					break;
+				case Keyboard::Numpad6:
+					if (current < primatives)
+					{
+						current++;
+					}
+					m_keyHeld = true;
+					break;
+				default:
+					break;
+				}
+			}
+
+
+
+			if (event.type == Event::KeyReleased && m_keyHeld)
+			{
+				switch (event.key.code)
+				{
+				case Keyboard::Space:
+				case Keyboard::Numpad4:
+				case Keyboard::Numpad6:
+				default:
+					m_keyHeld = false;
+					break;
+				}
+			}
 		}
 		update();
 		draw();
@@ -33,180 +182,209 @@ void Game::run()
 void Game::initialize()
 {
 	isRunning = true;
-
+	 // Uncomment for Part 2
+	 // ********************
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0);
 	glMatrixMode(GL_MODELVIEW);
-}
 
-void Game::update()
-{
-	cout << "Update up" << endl;
-		if (Keyboard::isKeyPressed(Keyboard::Up))
-		{
-			glTranslatef(0, 0.1f, 0);
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Down))
-		{
-			glTranslatef(0, -0.1f, 0);
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Left))
-		{
-			glTranslatef(-0.1f, 0, 0);
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Right))
-		{
-			glTranslatef(0.1f, 0, 0);
-		}
 
-		// X Axis
-		if (Keyboard::isKeyPressed(Keyboard::X))
-		{
-			glRotatef(1.0f, 0.1f, 0, 0);
-		} 
-		else if (Keyboard::isKeyPressed(Keyboard::C))
-		{
-			glRotatef(1.0f, -0.1f, 0, 0);
-		}
-
-		// Y Axis
-		if (Keyboard::isKeyPressed(Keyboard::S))
-		{
-			glRotatef(1.0f, 0, 0.1f, 0);
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::D))
-		{
-			glRotatef(1.0f, 0, -0.1f, 0);
-		}
-
-		// Z Axis
-		if (Keyboard::isKeyPressed(Keyboard::W))
-		{
-			glRotatef(1.0f, 0, 0, 0.1f);
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::E))
-		{
-			glRotatef(1.0f, 0, 0, -0.1f);
-		}
-
-		// scale on P
-		if (Keyboard::isKeyPressed(Keyboard::P))
-		{
-			glScalef(0.99f, 0.99f, 1);
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::O))
-		{
-			glScalef(1.01f, 1.01f, 1);
-		}
-}
-
-void Game::draw() 
-{ 
-	cout << "Draw up" << endl;
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glColor3f(1, 1, 1);
-	glBegin(GL_LINES); {
-		glVertex3f(2.0, 2.0, -60.0);
-		glVertex3f(-2.0, -2.0, -60.0);
-
-		glVertex3f(-2.0, -2.0, -60.0);
-		glVertex3f(2.0, -2.0, -60.0);
-
-		glVertex3f(2.0, -2.0, -60.0);
-		glVertex3f(2.0, 2.0, -60.0);
-
-	} glEnd();
-
-	glBegin(GL_POLYGON); {
-	glColor3f(0, 1, 0);
-	glVertex3f(0.0, 1.0, -75.0);
-
-	glColor3f(0, 0, 1);
-	glVertex3f(-1.0, -1.0, -75.0);
-
-	glColor3f(1, 0, 0);
-	glVertex3f(1.0, -1.0, -75.0);
-
-	} glEnd();
-	glBegin(GL_TRIANGLES); {
-		glColor3f(0, 1, 0);
-		glVertex3f(0.0, 1.0, -90.0);
-
-		glColor3f(0, 0, 1);
-		glVertex3f(-1.0, -1.0, -90.0);
-
-		glColor3f(1, 0, 0);
-		glVertex3f(1.0, -1.0, -90.0);
-
-	} glEnd();
-
-	glBegin(GL_TRIANGLE_STRIP); {
-		glColor3f(0, 1, 0);
-		glVertex3f(0.0, 1.0, -105.0);
-
-		glColor3f(0, 0, 1);
-		glVertex3f(-1.0, -1.0, -105.0);
-
-		glColor3f(1, 0, 0);
-		glVertex3f(1.0, -1.0, -105.0);
-
-	} glEnd();
-	
+	glNewList(index, GL_COMPILE);
 	glColor3f(1, 0, 0);
 	glPointSize(10.0f);
 	glBegin(GL_POINTS);
-	{	
-		glVertex3f(0.0, 5.0, -10.0);
+	{
+		glVertex3f(0.0, 1.0, -10.0f);
+	}
+	glEnd();
+	glEndList();
+
+	glNewList(index + 1, GL_COMPILE);
+	glBegin(GL_TRIANGLES); {
+		glColor3f(0, 1, 0);
+		glVertex3f(0.0, 1.0, -10.0f);
+
+		glColor3f(0, 0, 1);
+		glVertex3f(-2.0, -2.0, -10.0f);
+
+		glColor3f(1, 0, 0);
+		glVertex3f(2.0, -2.0, -10.0f);
+
 	} glEnd();
+	glEndList();
+
+	glNewList(index + 2, GL_COMPILE);
+	glBegin(GL_POLYGON); {
+	glColor3f(0, 1, 0);
+	glVertex3f(0.0, 1.0, -10.0f);
+
+	glColor3f(0, 0, 1);
+	glVertex3f(-1.0, -1.0, -10.0f);
 
 	glColor3f(1, 0, 0);
-	glPointSize(10.0f);
+	glVertex3f(1.0, -1.0, -10.0f);
+
+	} glEnd();
+	glEndList();
+
+	glNewList(index + 3, GL_COMPILE);
+	glBegin(GL_QUAD_STRIP);
+	{
+		glVertex3f(0.0, 0.0, -10.0f);
+		glVertex3f(1.0, 0.0, -10.0f);
+		glVertex3f(0.0, 1.0, -10.0f);
+		glVertex3f(1.0, 1.0, -10.0f);
+
+	} glEnd();
+	glEndList();
+
+	glNewList(index + 4, GL_COMPILE);
+	glColor3f(1, 1, 1);
+	glBegin(GL_LINES); {
+		glVertex3f(2.0, 2.0, -10.0f);
+		glVertex3f(-2.0, -2.0, -10.0f);
+
+		glVertex3f(-2.0, -2.0, -10.0f);
+		glVertex3f(2.0, -2.0, -10.0f);
+
+	} glEnd();
+	glEndList();
+
+	glNewList(index + 5, GL_COMPILE);
+	glBegin(GL_TRIANGLE_STRIP); {
+		glColor3f(0, 1, 0);
+		glVertex3f(0.0, 0.0, -10.0f);
+
+		glColor3f(0, 0, 1);
+		glVertex3f(-3.0, -3.0, -10.0f);
+
+		glColor3f(1, 0, 0);
+		glVertex3f(3.0, -3.0, -10.0f);
+
+	} glEnd();
+	glEndList();
+	
+	glNewList(index + 6, GL_COMPILE);
 	glBegin(GL_LINE_LOOP);
 	{
-		glVertex3f(0.0, 5.0, -30.0);
-		glVertex3f(0.0, 2.0, -30.0);
-		glVertex3f(2.0, 0.0, -30.0);
-		
-	} glEnd();
+		glVertex3f(0.0, 5.0, -10.0f);
+		glVertex3f(0.0, 2.0, -10.0f);
+		glVertex3f(2.0, 0.0, -10.0f);
 
+	} glEnd();
+	glEndList();
+
+	glNewList(index + 7, GL_COMPILE);
 	glColor3f(1, 0, 0);
 	glPointSize(10.0f);
 	glBegin(GL_TRIANGLE_FAN);
 	{
-		glVertex3f(0.0, 5.0, -30.0);
-		glVertex3f(0.0, 2.0, -30.0);
-		glVertex3f(2.0, 0.0, -30.0);
-		glVertex3f(-2.0, 0.0, -30.0);
-		glVertex3f(2.0, 5.0, -30.0);
+		glVertex3f(0.0, 5.0, -10.0f);
+		glVertex3f(0.0, 2.0, -10.0f);
+		glVertex3f(2.0, 0.0, -10.0f);
+		glVertex3f(-2.0, 0.0, -10.0f);
+		glVertex3f(2.0, 5.0, -10.0f);
 
 	} glEnd();
-
+	glEndList();
+	
+	glNewList(index + 8, GL_COMPILE);
 	glBegin(GL_QUADS);
 	{
-		glVertex3f(0.0, 0.0, -30.0);
-		glVertex3f(0.0, 1.0, -30.0);
-		glVertex3f(1.0, 1.0, -30.0);
-		glVertex3f(1.0, 0.0, -30.0);
+		glVertex3f(0.0, 0.0, -10.0f);
+		glVertex3f(0.0, 1.0, -10.0f);
+		glVertex3f(1.0, 1.0, -10.0f);
+		glVertex3f(1.0, 0.0, -10.0f);
 
 	} glEnd();
+	glEndList();
 
-	glBegin(GL_QUAD_STRIP);
+	 // Uncomment for Part 2
+	 // ********************
+}
+
+void Game::update()
+{
+	if (Keyboard::isKeyPressed(Keyboard::Up))
 	{
-		glVertex3f(0.0, 0.0, -45.0);
-		glVertex3f(1.0, 0.0, -45.0);
-		glVertex3f(0.0, 1.0, -45.0);
-		glVertex3f(1.0, 1.0, -45.0);
+		glTranslatef(0, 0.1f, 0);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Down))
+	{
+		glTranslatef(0, -0.1f, 0);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Left))
+	{
+		glTranslatef(-0.1f, 0, 0);
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Right))
+	{
+		glTranslatef(0.1f, 0, 0);
+	}
+	
+				// X Axis
+	if (Keyboard::isKeyPressed(Keyboard::X))
+	{
+		glRotatef(1.0f, 0.1f, 0, 0);
+	} 
+	else if (Keyboard::isKeyPressed(Keyboard::C))
+	{
+		glRotatef(1.0f, -0.1f, 0, 0);
+	}
+		
+				// Y Axis
+	if (Keyboard::isKeyPressed(Keyboard::S))
+	{
+		glRotatef(1.0f, 0, 0.1f, 0);
+	}
+	else if (Keyboard::isKeyPressed(Keyboard::D))
+	{
+		glRotatef(1.0f, 0, -0.1f, 0);
+	}
+		
+				// Z Axis
+	if (Keyboard::isKeyPressed(Keyboard::W))
+	{
+		glRotatef(1.0, 0, 0, 0.1f);
+	}
+	else if (Keyboard::isKeyPressed(Keyboard::E))
+	{
+		glRotatef(-1.0, 0, 0, -0.1f);
+	}
+		
+				// scale on P
+	if (Keyboard::isKeyPressed(Keyboard::P))
+	{
+		glScalef(0.99f, 0.99f, 1);
+	}
+	else if (Keyboard::isKeyPressed(Keyboard::O))
+	{
+		glScalef(1.01f, 1.01f, 1);
+	}
+	cout << "Update up" << endl;
+}
 
-	} glEnd();
+void Game::draw()
+{
+	 // Uncomment for Part 2
+	 // ********************
+	cout << "Draw up" << endl;
 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//Investigate Here!!!!!
+
+	cout << "Drawing Primative " << current << endl;
+	glCallList(current);
+	 // Uncomment for Part 2
+	 // ********************
 	window.display();
+
 }
 
 void Game::unload()
 {
 	cout << "Cleaning up" << endl;
 }
-
--- SFMLOpenGL/Game.h
+++ SFMLOpenGL/Game.h
@@ -1,8 +1,10 @@
 #include <iostream>
-#include <SFML/Window.hpp>
+//#include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
+#include <SFML/Graphics.hpp>
 #include <gl/GL.h>
 #include <gl/GLU.h>
+
 
 using namespace std;
 using namespace sf;
@@ -20,15 +22,25 @@ private:
 	void update();
 	void draw();
 	void unload();
+	void updateText();
 
-	// // Uncomment for Part 2
-	// // ********************
-	// const int primatives;
+	 // Uncomment for Part 2
+	 // ********************
+	 const int primatives;
 
-	//GLuint index;
-	//Clock clock;
-	//Time elapsed;
+	GLuint index;
+	Clock clock;
+	Time elapsed;
 
-	//float rotationAngle = 0.0f;
-	// // ********************
+	float rotationAngle = 0.0f;
+	 // ********************
+
+	// Uncomment for Part 2
+	// ********************
+	bool flip = false;
+	int current = 1;
+	// ********************
+
+	bool m_keyHeld{ false };
+
 };
